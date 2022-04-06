7d94d720ca4f7f54fe16936fcd15c6bdde921d68
David <david.krtolica8@gmail.com>
2022-04-06 18:29:26 +0200

    Changed the node-fetch version to be able to use require - started working on currency testing (integration), issues with currency class

--- Currency.js
+++ Currency.js
@@ -2,16 +2,30 @@ const fetch = require('node-fetch');
 
 class Currency {
     constructor(baseCurrency) {
-        this.baseCurrency = baseCurrency;
+        if (typeof(baseCurrency) == 'string' && baseCurrency.length == 3) {
+            this.baseCurrency = baseCurrency;
+        } else {
+            throw 'Invalid value for base currency - must be 3-letter format!';
+        }
     }
     async convert(amountToConvert, destinationCurrency) {
-        const response = await fetch(`https://api.currencyapi.com/v3/latest?apikey=yKiW7APVWuQjXeXH3m2TTFLn8dtk0LbsvXBvSOmv&base_currency=${this.baseCurrency}`);
-        const json = await response.json();
-        const currenciesData = json.data;
-        const finalResult = currenciesData[destinationCurrency].value * amountToConvert;
-        return `${amountToConvert}${this.baseCurrency} is equal to ${finalResult}${destinationCurrency}`;
+        /* if (amountToConvert > 0) {
+            if (typeof(destinationCurrency) == 'string' && destinationCurrency.length == 3) { */
+                const response = await fetch(`https://api.currencyapi.com/v3/latest?apikey=yKiW7APVWuQjXeXH3m2TTFLn8dtk0LbsvXBvSOmv&base_currency=${this.baseCurrency}`);
+                const json = await response.json();
+                const currenciesData = json.data;
+                const finalResult = currenciesData[destinationCurrency].value * amountToConvert;
+                return `${amountToConvert}${this.baseCurrency} is equal to ${finalResult}${destinationCurrency}`;
+            /*} else {
+                throw 'Incorrect destination currency input!';
+            }
+        } else {
+            throw 'Invalid value for amount to convert - must be a positive number higher than 0!';
+        } */
     }
 }
 
+/* let testCurr = new Currency('USD');
+console.log(await testCurr.convert(100, 'EUR')); */
 
 module.exports = Currency;
---
+++ currency.test.js
@@ -0,0 +1,21 @@
+const Currency = require('./currency.js');
+
+//TESTING THROW ERRORS
+test('incorrect base currency value to throw error', () => {
+    expect(() => {
+        new Currency('danish krone');
+    }).toThrow('Invalid value for base currency - must be 3-letter format!');
+});
+
+test('incorrect base currency value to throw error', () => {
+    expect(() => {
+        new Currency('US DOLLAR');
+    }).toThrow('Invalid value for base currency - must be 3-letter format!');
+});
+
+/* test('incorrect amount value conversion to throw error', () => {
+    expect(() => {
+        const testCurr = new Currency('USD');
+        testCurr.convert(-150, 'EUR');
+    }).toThrow('Invalid value for amount to convert - must be a positive number higher than 0!');
+}); */
